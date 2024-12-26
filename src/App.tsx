e218b91c8f87406ce4b602ac7c8bb99a925a0115
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-12-26 10:11:18 +0100

    Initial commit for landing page. To include transitions afterwards.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Homepage from "./components/Homepage";
+
+const App = () => {
+  return (
+    <>
+      <div className="h-dvh">
+        <Homepage />
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="bg-black grid ">Hero</div>
+      <section className="bg-black flex top-0 z-0">Section</section>
+    </>
+  );
+};
+export default Hero;
