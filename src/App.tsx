3c2ec62047757ea72b9b58582da3e134029800ac
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-12-27 15:34:46 +0100

    Mobile responsive for landing page (code clean-up: tsx error)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Homepage from "./components/Homepage";
-import Landing from "./components/Landing";
 
 const App = () => {
   return (
     <>
       <div className="h-dvh bg-black">
         <Homepage />
-        {/* <Landing /> */}
       </div>
     </>
   );
