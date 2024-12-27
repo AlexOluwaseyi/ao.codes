ee203298d1fa778afb313ee54190cc6efab8f6c9
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-12-27 15:45:49 +0100

    Fix icons and added margin for mobile view.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,7 +2,7 @@ import profile from "../assets/images/headshot profile2.jpeg";
 
 const Landing = () => {
   return (
-    <div className="landing-page flex bg-black h-full max-w-4xl items-center justify-centers mx-auto">
+    <div className="landing-page flex mx-4 bg-black h-full max-w-4xl items-center justify-centers md:mx-auto">
           <div className="grid w-full gap-4 grid-cols-1 grid-rows-4 rounded-lg shadow-md h-2/3 bg-black p-2 md:grid md:h-max md:w-full md:gap-4 md:grid-cols-3 md:grid-rows-4">
             <div className="col-span-1 row-span-4 md:col-span-1 md:row-span-4 rounded-lg shadow-md flex items-center justify-center ">
               <img
