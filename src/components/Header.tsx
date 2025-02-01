84078d684615536b428614e63dd44fba1b1383a6
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-02-01 16:21:43 +0100

    Fixed navbar positioning and added close button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,42 +21,65 @@ const Header = () => {
         <div className="col-span-1 grid grid-cols-1 gap-4">
           <button
             id="mobile-open-button"
-            className="text-regular text-3xl sm:hidden focus:outline-none"
+            // className="text-regular text-3xl justify-self-end sm:hidden focus:outline-none"
+            className={`${
+              isMobileMenuOpen
+                ? "hidden"
+                : "text-regular text-3xl justify-self-end sm:hidden focus:outline-none"
+            }`}
             onClick={toggleMobileMenu}
           >
             &#9776;
           </button>
+          <button
+            id="mobile-close-button"
+            className={`${
+              isMobileMenuOpen
+                ? "text-reguler text-3xl justify-self-end z-10"
+                : "hidden"
+            }`}
+            // className="text-regular text-3xl justify-self-end sm:hidden focus:outline-none"
+            onClick={toggleMobileMenu}
+          >
+            &times;
+          </button>
           <nav
             className={`${
               isMobileMenuOpen
-                ? "flex h-dvh w-dvw top-0 bg-black flex-col items-center justify-between text-center"
+                ? "flex h-dvh w-dvw top-0 left-0 pt-16 absolute bg-black flex-col items-center text-center"
                 : "hidden sm:flex flex-row space-x-8 text-xl-text-regular items-center justify-evenly"
             }`}
             aria-label="main"
           >
             <a
               href="#Stories"
-              className="hover:text-red-400 hover:underline hover: underline-offset-2"
+              className="py-4 sm:py-0 hover:text-red-400 hover:underline hover:underline-offset-2"
             >
               Stories
             </a>
             <a
               href="#LinkedIn"
-              className="hover:text-red-400 hover:underline hover: underline-offset-2"
+              className="py-4 sm:py-0 hover:text-red-400 hover:underline hover:underline-offset-2"
             >
               LinkedIn
             </a>
             <a
               href="#GitHub"
-              className="hover:text-red-400 hover:underline hover: underline-offset-2"
+              className="py-4 sm:py-0 hover:text-red-400 hover:underline hover:underline-offset-2"
             >
               GitHub
             </a>
             <a
               href="#Twitter"
-              className="hover:text-red-400 hover:underline hover: underline-offset-2"
+              className="py-4 sm:py-0 hover:text-red-400 hover:underline hover: underline-offset-2"
             >
               X
+            </a>
+            <a
+              href="#quote"
+              className="py-4 sm:py-0 sm:hidden hover:text-red-400 hover:underline hover: underline-offset-2"
+            >
+              Get a quote
             </a>
           </nav>
         </div>
