5d6e6782ef2d5c18751e46b758adcd4a35594a3a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-02-20 07:54:11 +0100

    Fix: Browser Router issues in production build after deployment to Netlify.
    Changed Router from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import BlogsPage from "./components/BlogsPage";
 import BlogPost from "./components/BlogPost";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
